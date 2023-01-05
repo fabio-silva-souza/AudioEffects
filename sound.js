21835451364cdccdbdb2ee6f1d21a87f319b25c7
fabio-silva-souza <silvafabiosouza@hotmail.com>
2023-01-04 22:46:48 -0300

    add ul

--- sound.js
+++ sound.js
@@ -1,5 +1,6 @@
 const file = document.getElementById('fileInput');
-
+// const beforeBtn = document.getElementById('before');
+// const afterBtn = document.getElementById('after');
 const canvas = document.getElementById('effects');
 const ctx = canvas.getContext('2d');
 console.log(ctx)
@@ -58,36 +59,36 @@ file.addEventListener('change', function(){
 });
 
 
-// function drawVisualiser(bufferLenght, x, barWidht, barHeight, dataArray){
-
-//     for (let i = 0; i < bufferLenght; i++){
-//         barHeight = dataArray[i] * 1.5;
-//         ctx.save();
-//         ctx.translate(canvas.width/2, canvas.height/2);
-//         ctx.rotate(i + Math.PI * 8 / bufferLenght);
-//         const hue = i * 15; //hsl hue,saturation,lightness
-//         // ctx.fillStyle = 'blueviolet';
-//         // ctx.fillRect(10, 5, barWidht/2, barHeight*2);
-//         ctx.fillStyle = 'hsl(' + hue + ',100%, 50%)';
-//         ctx.fillRect(0, 0, barWidht, barHeight); //(8, 5, barWidht/2, barHeight*2)
-//         x += barWidht;
-//         ctx.restore();
-//     }
-// }
-
 function drawVisualiser(bufferLenght, x, barWidht, barHeight, dataArray){
+
     for (let i = 0; i < bufferLenght; i++){
-        barHeight = dataArray[i] * 1;
+        barHeight = dataArray[i] * 1.5;
         ctx.save();
         ctx.translate(canvas.width/2, canvas.height/2);
-        ctx.rotate(i * Math.PI * 5 / bufferLenght);
-        const hue = i * 80; //hsl hue,saturation,lightness 'i * 2'
+        ctx.rotate(i + Math.PI * 8 / bufferLenght);
+        const hue = i * 15; //hsl hue,saturation,lightness
+        // ctx.fillStyle = 'blueviolet';
+        // ctx.fillRect(10, 5, barWidht/2, barHeight*2);
         ctx.fillStyle = 'hsl(' + hue + ',100%, 50%)';
-        ctx.fillRect(0, 0, barWidht, -barHeight);//(5, 5, barWidht, barHeight)
-        // x += barWidht;
+        ctx.fillRect(0, 0, barWidht, barHeight); //(8, 5, barWidht/2, barHeight*2)
+        x += barWidht;
         ctx.restore();
     }
 }
+
+// function drawVisualiser(bufferLenght, x, barWidht, barHeight, dataArray){
+//     for (let i = 0; i < bufferLenght; i++){
+//         barHeight = dataArray[i] * 1;
+//         ctx.save();
+//         ctx.translate(canvas.width/2, canvas.height/2);
+//         ctx.rotate(i * Math.PI * 5 / bufferLenght);
+//         const hue = i * 80; //hsl hue,saturation,lightness 'i * 2'
+//         ctx.fillStyle = 'hsl(' + hue + ',100%, 50%)';
+//         ctx.fillRect(0, 0, barWidht, -barHeight);//(5, 5, barWidht, barHeight)
+//         // x += barWidht;
+//         ctx.restore();
+//     }
+// }
 
 // function drawVisualiser(bufferLenght, x, barWidht, barHeight, dataArray){
 //     for (let i = 0; i < bufferLenght; i++){
@@ -139,3 +140,10 @@ function drawVisualiser(bufferLenght, x, barWidht, barHeight, dataArray){
 //         ctx.restore();
 //     }
 // }
+
+const optionsVisu = [drawVisuOp1, drawVisuOp2, drawVisuOp3, drawVisuOp4]
+
+document.getElementById('after').addEventListener('click', () => {
+    drawVisuOp1();
+    
+})
